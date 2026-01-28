6eeea429648d9b55d09b5e33d3bfd91b82ab79f6
Louis <3095746912@qq.com>
2026-01-28 20:12:10 +0800

    完善InetAddress类

--- src/socket/InetAddress.cc
+++ src/socket/InetAddress.cc
@@ -1,1 +1,37 @@
 #include "InetAddress.h"
+
+#include <arpa/inet.h>
+#include <cstdint>
+
+namespace net {
+InetAddress::InetAddress(uint16_t port, bool loopbackOnly) {
+    addr_.sin_family = AF_INET;
+    // 监听回环地址还是监听所有地址
+    in_addr_t ip = loopbackOnly ? htonl(INADDR_LOOPBACK) : htonl(INADDR_ANY);
+    addr_.sin_addr.s_addr = ip;
+    addr_.sin_port = htons(port);
+}
+InetAddress::InetAddress(const std::string& ip, uint16_t port) {
+    addr_.sin_family = AF_INET;
+    addr_.sin_port = htons(port);
+    inet_pton(AF_INET, ip.c_str(), &addr_.sin_addr);
+}
+InetAddress::InetAddress(const struct sockaddr_in& addr) : addr_(addr) {
+}
+
+// 获取地址
+std::string InetAddress::toIp() const {
+    char buf[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &addr_.sin_addr, buf, sizeof(buf));
+    return buf;
+}
+uint16_t InetAddress::toPort() const {
+    return ntohs(addr_.sin_port);
+}
+std::string InetAddress::toIpPort() const {
+    char buf[INET_ADDRSTRLEN];
+    inet_ntop(AF_INET, &addr_.sin_addr, buf, sizeof(buf));
+    uint16_t port = ntohs(addr_.sin_port);
+    return std::string(buf) + ":" + std::to_string(port);
+}
+}  // namespace net
--- src/socket/InetAddress.h
+++ src/socket/InetAddress.h
@@ -2,22 +2,30 @@
 
 #include <arpa/inet.h>
 
+#include <cstdint>
 #include <string>
 
 namespace net {
 class InetAddress {
    public:
-    // 构造析构
-    InetAddress(std::string ip, int port);
-    InetAddress(const struct sockaddr_in& addr);
-    ~InetAddress();
+    // 默认为0，表示让操作系统自动分配一个可用端口
+    explicit InetAddress(uint16_t port = 0, bool loopbackOnly = false);
+    InetAddress(const std::string& ip, uint16_t port);
+    explicit InetAddress(const struct sockaddr_in& addr);
+    
+    // 获取地址
+    std::string toIp() const;
+    uint16_t toPort() const;
+    std::string toIpPort() const;
 
-    // 获取IP地址
-    std::string ip() const;
-    // 获取端口号
-    int port() const;
-    // 获取struct sockaddr_in指针
-    const sockaddr_in* sockaddr() const;
+    // 获取原始的sockaddr指针
+    const struct sockaddr* getSockaddr() const {
+        return reinterpret_cast<const struct sockaddr*>(&addr_);
+    }
+    // 设置地址
+    void setSockAddrInet(const struct sockaddr_in& addr) {
+        addr_ = addr;
+    }
 
    private:
     struct sockaddr_in addr_ {};
