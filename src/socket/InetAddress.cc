6d80f0ea31bafb1585eb93e63a7e33b0950dc8c4
Louis <3095746912@qq.com>
2026-02-11 19:44:50 +0800

    完成对InetAddress类的测试

--- src/socket/InetAddress.cc
+++ src/socket/InetAddress.cc
@@ -4,6 +4,7 @@
 #include <cstdint>
 
 namespace net {
+// 仅端口构造，可以指定是否本地回环
 InetAddress::InetAddress(uint16_t port, bool loopbackOnly) {
     addr_.sin_family = AF_INET;
     // 监听回环地址还是监听所有地址
@@ -11,23 +12,27 @@ InetAddress::InetAddress(uint16_t port, bool loopbackOnly) {
     addr_.sin_addr.s_addr = ip;
     addr_.sin_port = htons(port);
 }
+// IP，端口构造
 InetAddress::InetAddress(const std::string& ip, uint16_t port) {
     addr_.sin_family = AF_INET;
     addr_.sin_port = htons(port);
     inet_pton(AF_INET, ip.c_str(), &addr_.sin_addr);
 }
+// sockaddr_in构造
 InetAddress::InetAddress(const struct sockaddr_in& addr) : addr_(addr) {
 }
 
-// 获取地址
+// 获取IP
 std::string InetAddress::toIp() const {
     char buf[INET_ADDRSTRLEN];
     inet_ntop(AF_INET, &addr_.sin_addr, buf, sizeof(buf));
     return buf;
 }
+// 获取端口
 uint16_t InetAddress::toPort() const {
     return ntohs(addr_.sin_port);
 }
+// 获取IP:端口
 std::string InetAddress::toIpPort() const {
     char buf[INET_ADDRSTRLEN];
     inet_ntop(AF_INET, &addr_.sin_addr, buf, sizeof(buf));
--- src/socket/InetAddress.h
+++ src/socket/InetAddress.h
@@ -8,14 +8,19 @@
 namespace net {
 class InetAddress {
    public:
-    // 默认为0，表示让操作系统自动分配一个可用端口
-    explicit InetAddress(uint16_t port = 0, bool loopbackOnly = false);
+    // 仅端口构造，可以指定是否本地回环
+    explicit InetAddress(uint16_t port = 0,
+                         bool loopbackOnly = false);  // port默认为0，表示让操作系统自动分配一个可用端口
+    // IP，端口构造
     InetAddress(const std::string& ip, uint16_t port);
+    // sockaddr_in构造
     explicit InetAddress(const struct sockaddr_in& addr);
-    
-    // 获取地址
+
+    // 获取IP
     std::string toIp() const;
+    // 获取端口
     uint16_t toPort() const;
+    // 获取IP:端口
     std::string toIpPort() const;
 
     // 获取原始的sockaddr指针
