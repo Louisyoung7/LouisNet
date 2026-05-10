a555d5905d73a9786e603cf0afeb412fcb2345f4
Louis <3095746912@qq.com>
2026-05-10 15:59:19 +0800

    基本完成对Connector类的编写（start, stop, restart等等）

---
+++ src/net/Connector.cc
@@ -0,0 +1,112 @@
+#include "Connector.h"
+
+#include <cassert>
+
+#include "Socket.h"
+#include "reactor/Channel.h"
+#include "reactor/EventLoop.h"
+
+using namespace net;
+
+// 启动连接
+void Connector::start() {
+    connected_ = true;
+    // runInLoop 强制在Loop线程执行
+    // 如果当前线程不是Loop线程，会进入Loop线程的任务队列等待执行
+    // 如果当前线程是Loop线程，会立即执行
+    loop_->runInLoop([this]() { startInLoop(); });
+}
+
+// 停止连接
+void Connector::stop() {
+    connected_ = false;
+    // queueInLoop 强制在Loop线程的任务队列延迟执行
+    loop_->queueInLoop([this]() { stopInLoop(); });
+}
+
+// 重启连接，必须在事件循环中调用
+void Connector::restart() {
+    assert(loop_->isInLoopThread());
+    // 重置状态
+    state_ = State::kDisconnected;
+    connected_ = true;
+    startInLoop();
+}
+
+// Loop线程启动连接
+void Connector::startInLoop() {
+    assert(loop_->isInLoopThread());
+    assert(state_ == State::kDisconnected);
+    if (connected_) { connect(); }
+}
+
+// Loop线程停止连接
+void Connector::stopInLoop() {
+    assert(loop_->isInLoopThread());
+    if (state_ == State::kConnecting) {
+        state_ = State::kDisconnected;
+        int sockfd = getSocket();
+        assert(sockfd >= 0);
+        ::close(sockfd);
+    }
+}
+
+// 创建socket并尝试连接对端
+void Connector::connect() {
+    // 创建非阻塞的socket
+    int sockfd = createNonblockingSocket();
+    // 连接对端
+    if (connectPeer(sockfd, serverAddr_) == 0) {
+        connecting(sockfd);
+    } else {
+        ::close(sockfd);
+    }
+}
+
+// 连接过程中的操作
+void Connector::connecting(int sockfd) {
+    state_ = State::kConnecting;
+    // 创建Channel，用于监听发起连接事件
+    channel_.reset(new reactor::Channel(loop_, sockfd));
+    // 关联Channel和Connector，确保在Channel事件发生时能够调用Connector的回调函数
+    channel_->tie(shared_from_this());
+    channel_->setWriteCallback([this]() { handleWrite(); });
+    channel_->setErrorCallback([this]() { handleError(); });
+    // 启用写事件，关心EPOLLOUT
+    channel_->enableWrite();
+}
+
+// 获取已连接的socket fd，并从EventLoop中移除Channel
+int Connector::getSocket() {
+    channel_->disableAll();
+    channel_->remove();
+    int sockfd = channel_->fd();
+    // 不能在此处重置Channel，当前可能位于Channel的handleEvents中
+    // 需要 queueInLoop 强制在Loop线程的任务队列延迟执行
+    loop_->queueInLoop([this]() { resetChannel(); });
+    return sockfd;
+}
+
+// 重置Channel
+void Connector::resetChannel() { channel_.reset(); }
+
+// 处理写事件
+// 取出已连接的socket fd，调用新连接回调函数
+void Connector::handleWrite() {
+    if (state_ == State::kConnecting) {
+        state_ = State::kConnected;
+        int sockfd = getSocket();
+        assert(sockfd >= 0);
+        newConnectionCallback_(sockfd);
+    } else {
+        assert(state_ == State::kDisconnected);
+    }
+}
+// 处理错误事件
+// 取出已连接的socket fd，关闭Channel和socket
+void Connector::handleError() {
+    assert(state_ == State::kConnecting);
+    int sockfd = getSocket();
+    assert(sockfd >= 0);
+    ::close(sockfd);
+}
