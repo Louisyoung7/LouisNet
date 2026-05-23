6675c299b4d2d9be21390523df63a722b079bf16
Louis <3095746912@qq.com>
2026-05-23 20:13:57 +0800

    修改日志的默认级别

--- src/log/Logger.cc
+++ src/log/Logger.cc
@@ -4,6 +4,7 @@
 
 #include "spdlog/async.h"
 #include "spdlog/sinks/stdout_color_sinks.h"
+#include "spdlog/spdlog.h"
 
 using namespace spdlog;
 
@@ -13,9 +14,10 @@ void init() {
     init_thread_pool(8192, 2);
 
     auto console_sink = std::make_shared<sinks::stdout_color_sink_mt>();
-    console_sink->set_level(level::info);
+    console_sink->set_level(level::trace);  // 设置 sink 级别为 trace
 
     logger_ = std::make_shared<async_logger>("server", console_sink, thread_pool(),
                                              spdlog::async_overflow_policy::discard_new);
+    logger_->set_level(level::trace);  // 设置 logger 级别为 trace
     set_default_logger(logger_);
 }
