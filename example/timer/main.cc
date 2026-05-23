70afaca59f5416c14d664671ad277a3f7754e630
Louis <3095746912@qq.com>
2026-05-23 21:23:02 +0800

    验证定时器的添加，取消定时任务功能

---
+++ example/timer/main.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+
+#include "log/Logger.h"
+#include "net/reactor/EventLoop.h"
+#include "timer/Timer.h"
+
+using namespace net::reactor;
+using namespace timer;
+
+int main() {
+    init();                                        // 先初始化日志系统
+    setLogLevel(spdlog::level::level_enum::info);  // 设置日志级别
+
+    EventLoop loop;  // EventLoop 构造，在日志系统初始化之后调用
+
+    info("[EventLoop] started.\n\n");
+
+    loop.runAfter(timer::Duration(20000000), timer::TimerCallback([] { std::cout << "Hello, World!" << std::endl; }));
+
+    loop.runAt(timer::Timestamp::now() + timer::Duration(20000000),
+               timer::TimerCallback([] { std::cout << "Hello, World!" << std::endl; }));
+
+    auto timerId = loop.runEvery(timer::Duration(20000000),
+                                 timer::TimerCallback([] { std::cout << "Hello, World!" << std::endl; }));
+
+    loop.runAfter(timer::Duration(200000000), [timerId, &loop]() { loop.cancel(timerId); });
+
+    loop.loop();
+
+    shutdown();
+
+    return 0;
+}
