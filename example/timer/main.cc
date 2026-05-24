22bed00e67dc559dd10e4e20c3f11be8a698eca4
Louis <3095746912@qq.com>
2026-05-24 09:45:44 +0800

    完善Timer的基本example
    正确执行runAfter、runAt、runEvery等定时器API

--- example/timer/main.cc
+++ example/timer/main.cc
@@ -15,15 +15,18 @@ int main() {
 
     info("[EventLoop] started.\n\n");
 
-    loop.runAfter(timer::Duration(20000000), timer::TimerCallback([] { std::cout << "Hello, World!" << std::endl; }));
+    loop.runAfter(timer::Duration(std::chrono::seconds(2)),
+                  timer::TimerCallback([] { std::cout << "Hello, World!" << std::endl; }));
 
-    loop.runAt(timer::Timestamp::now() + timer::Duration(20000000),
+    loop.runAt(timer::Timestamp::now() + timer::Duration(std::chrono::seconds(2)),
                timer::TimerCallback([] { std::cout << "Hello, World!" << std::endl; }));
 
-    auto timerId = loop.runEvery(timer::Duration(20000000),
+    auto timerId = loop.runEvery(timer::Duration(std::chrono::seconds(2)),
                                  timer::TimerCallback([] { std::cout << "Hello, World!" << std::endl; }));
 
-    loop.runAfter(timer::Duration(200000000), [timerId, &loop]() { loop.cancel(timerId); });
+    loop.runAfter(timer::Duration(std::chrono::seconds(8)), [timerId, &loop]() { loop.cancel(timerId); });
+
+    loop.runAfter(timer::Duration(std::chrono::seconds(10)), [&loop]() { loop.quit(); });
 
     loop.loop();
 
